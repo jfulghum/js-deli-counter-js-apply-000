c51e0bc240d45da2cc092ed9bd30d0c47f521c1a
Johanna Fulghum <johanna.fulghum@gmail.com>
2017-10-11 19:02:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function takeANumber(katzDeliLine, newPerson){
+  if (katzDeliLine){
+    katzDeliLine.push(`${newPerson}`);
+  }
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length < 1){
+    return "There is nobody waiting to be served!"
+  }
+  return `Currently serving ${katzDeliLine[0]}`
+}
+
+function currentLine(katzDeli){
+  var line = [];
+  for(var i = 0; i < katzDeli.length; i++){
+    line.push(` `+[i+1]+`. ` + katzDeli[i])
+  }
+  if (katzDeli.length < 1){
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
